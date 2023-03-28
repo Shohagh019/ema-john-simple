186a4a8cfa814e27ee026aad05312a509e0135e0
Shohagh019 <shohaghmunna@gmail.com>
2023-03-28 15:27:11 +0600

    cart set to local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,17 +4,21 @@ import "./cart.css";
 const Cart = (props) => {
     const cart = props.cart;
     let total= 0;
+    let totalShipping = 0;
     for(const product of cart){
         total= total+ product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+    const tax = total*7/100;
+    const grandTotal = total + totalShipping + tax;
   return (
     <div className="cart">
       <h3>Order Summary</h3>
       <p>Selected Items: {cart.length}</p>
       <p>Total Prcie: ${total}</p>
-      <p>Total Shipping Charge: </p>
-      <p>Tax: </p>
-      <h6>Grand Total: </h6>
+      <p>Shipping: ${totalShipping} </p>
+      <p>Tax: {tax.toFixed(2)}</p>
+      <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
     </div>
   );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Products/Product";
 import "./Shop.css";
@@ -12,9 +13,18 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  useEffect(()=>{
+    const storedCart = getShoppingCart();
+    console.log(storedCart);
+  },[])
+
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id)
+    
+    
+    
   };
   return (
     <div className="shop-container">
